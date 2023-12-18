c8a34cdce628a2591bdc8ae660224a3ba7c000c6
wkimunya <136619749+wkimunya@users.noreply.github.com>
2023-12-18 23:44:14 +0300

    initiating the restaurant modrl

--- python-code-challenge-pizzas/code-challenge/app/models.py
+++ python-code-challenge-pizzas/code-challenge/app/models.py
@@ -3,8 +3,27 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Restaurant(db.Model):
-    __tablename__ = 'Restaurant'
+    __tablename__ = 'restaurants'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
 
-# add any models you may need. 
+    pizzas = db.relationship('Pizza', secondary='restaurant_pizzas', back_populates='restaurants')
+
+# class Pizza(db.Model):
+#     __tablename__ = 'pizzas'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(100), nullable=False)
+
+#     restaurants = db.relationship('Restaurant', secondary='restaurant_pizzas', back_populates='pizzas')
+
+# class RestaurantPizza(db.Model):
+#     __tablename__ = 'restaurant_pizzas'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+#     pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+
+#     restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
+#     pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
